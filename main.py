684c55fc3f41c32b54f020fb839135e76b6db299
hidemic06-eng <hidemic06@gmail.com>
2026-04-05 10:31:46 +0900

    iPhone Makiでテスト

--- main.py
+++ main.py
@@ -159,7 +159,7 @@ if "password_correct" not in st.session_state:
             detected_user = "Maki"
         elif "iPhone" in ua:
             os_info = "iPhone"
-            detected_user = "Hide"
+            detected_user = "Maki"
         elif "iPad" in ua:
             os_info = "iPad"
             detected_user = "Hide"
